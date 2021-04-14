b8a629096ec7aa7b3f9b8007ae3e5cb74a7ebc71
JenilGajjar20 <68738624+JenilGajjar20@users.noreply.github.com>
2021-04-14 14:17:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let colors = ['green', 'red', 'rgb(114, 133, 100)', '#a1b0c5', 'yellow', 'blue', 'cyan'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    var randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
